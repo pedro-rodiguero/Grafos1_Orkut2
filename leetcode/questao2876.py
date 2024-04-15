b7727a2a6aed9b968cddc667475c5a0e846a4405
pedro-rodiguero <190139323@aluno.unb.br>
2024-04-15 15:37:06 -0300

    final answer question 2876

--- leetcode/questao2876.py
+++ leetcode/questao2876.py
@@ -1,23 +1,18 @@
 class Solution:
     def countVisitedNodes(self, edges):
         n = len(edges)
-        memo = [-1] * n
         visited = [False] * n
+        count = [0] * n
 
-        def dfs(node, path):
-            if visited[node]:
-                return 0
-            if node in path:
-                return len(path) - path.index(node)
-            if memo[node] != -1:
-                return memo[node]
-            path.append(node)
-            visited[node] = True
-            memo[node] = dfs(edges[node], path) + 1
-            path.pop()
-            return memo[node]
+        for i in range(n):
+            current = i
+            while not visited[current]:
+                visited[current] = True
+                count[i] += 1
+                current = edges[current]
+            visited = [False] * n
 
-        return [dfs(i, []) for i in range(n)]
+        return count
 
 
 ## LeetCode Question 2876 - Count Visited Nodes - https://leetcode.com/problems/count-visited-nodes-in-a-directed-graph/
